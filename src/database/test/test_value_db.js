d727f454c1bfd72924cd278dbada33bf8933de72
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-09-21 11:00:06 -0300

    adjust test tables

--- src/database/test/test_value_db.js
+++ src/database/test/test_value_db.js
@@ -22,7 +22,7 @@ Database.then( async (db) => {
     
     //abaixo a função que usa o arquivo test_value_db.js para inserir todos os valores acima no banco de dados
     //será comentado apenas para que a funçãode consulta seja usada, caso necessário reinserir dados acima, descomentar afunção abaixo
-    //await test_insert_db(db, { testEmployee })
+    await test_insert_db(db, { testEmployee })
 
     //função abaixo realiza consulta na tabela db_employee
     const consulting = await db.all('SELECT * FROM db_employee')
